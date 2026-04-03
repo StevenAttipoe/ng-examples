17eed54a88205d55ddb1d68e01c89782311bd751
marktechson <2554588+MarkTechson@users.noreply.github.com>
2026-04-03 15:50:39 -0500

    feat: renaming project

---
+++ firebase-ai-logic-angular-example/src/environments/environment.development.ts
@@ -0,0 +1,19 @@
+/*!
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA2KPSQBH6Nv5mMOKuc924mNK0kHI0hfnw",
+    authDomain: "vertex-ai-example-10e20.firebaseapp.com",
+    projectId: "vertex-ai-example-10e20",
+    storageBucket: "vertex-ai-example-10e20.firebasestorage.app",
+    messagingSenderId: "636067503499",
+    appId: "1:636067503499:web:ef1e94e2c76ecf9735638e",
+    measurementId: "G-77FDLM9BM2",
+  },
+};
